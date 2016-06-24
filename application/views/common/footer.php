f678e946cc8d1b3e20526b143626c7d80fd8dc6c
Lyndon1994 <wuhulinyi@sina.com>
2016-06-24 09:24:31 +0800

    分享按钮

--- application/views/common/footer.php
+++ application/views/common/footer.php
@@ -10,6 +10,9 @@
   ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
 <script src="<?php echo base_url(); ?>source/js/bootstrap.min.js"></script>
+
+//分享按钮
+<script>window._bd_share_config={"common":{"bdSnsKey":{},"bdText":"","bdMini":"1","bdMiniList":["mshare","qzone","tsina","bdysc","weixin","renren","kaixin001","tieba","douban","sqq","thx","ibaidu","meilishuo","mogujie","diandian","huaban","duitang","hx","fx","youdao","sdo","qingbiji","people","xinhua","mail","yaolan","ty","fbook","linkedin","evernotecn","copy","print"],"bdPic":"","bdStyle":"0","bdSize":"16"},"slide":{"type":"slide","bdImg":"1","bdPos":"right","bdTop":"100"},"image":{"viewList":["weixin","qzone","sqq","tsina","renren","tieba"],"viewText":"分享到：","viewSize":"16"}};with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];</script>
 </body>
 
 </html>
